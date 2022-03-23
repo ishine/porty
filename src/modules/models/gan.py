cc5dba5fd3801f9fd3216323d06331cc81139a63
pc3090 <ilikeniku@icloud.com>
2022-03-24 02:25:19 +0900

    initial

--- src/modules/models/gan.py
+++ src/modules/models/gan.py
@@ -4,10 +4,19 @@ import torch.nn.functional as F
 from torch.nn.utils import weight_norm, remove_weight_norm
 
 from .layers import ResBlock
-from .utils import init_weights, get_padding
 
 
 LRELU_SLOPE = 0.1
+
+
+def init_weights(m, mean=0.0, std=0.01):
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        m.weight.data.normal_(mean, std)
+
+
+def get_padding(kernel_size, dilation=1):
+    return int((kernel_size * dilation - dilation) / 2)
 
 
 class Generator(torch.nn.Module):
