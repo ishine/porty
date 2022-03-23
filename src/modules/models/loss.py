b0e4fd79b2eb90af9704ac8f58d4ce4773cf6a10
pc3090 <ilikeniku@icloud.com>
2022-03-24 02:35:37 +0900

    initial

--- src/modules/models/loss.py
+++ src/modules/models/loss.py
@@ -21,13 +21,13 @@ def fm_loss(fmap_r, fmap_g):
     loss = 0
     for dr, dg in zip(fmap_r, fmap_g):
         for rl, gl in zip(dr, dg):
-            loss += torch.mean(torch.abs(rl.detach() - gl))
+            loss += (rl.detach() - gl).abs().mean()
     return 2 * loss
 
 
 def kl_loss(z_p, logs_q, m_p, logs_p, z_mask):
     kl = logs_p - logs_q - 0.5
-    kl += 0.5 * ((z_p - m_p)**2) * torch.exp(-2. * logs_p)
+    kl += 0.5 * (z_p - m_p).pow(2) * (-2. * logs_p).exp()
     kl = torch.sum(kl * z_mask)
     l = kl / torch.sum(z_mask)
     return l
