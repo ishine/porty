0585175f697eb38dbd1dc1d77224046cb33e534e
pc3090 <ilikeniku@icloud.com>
2022-03-24 02:07:19 +0900

    initial

--- src/modules/models/gan.py
+++ src/modules/models/gan.py
@@ -128,12 +128,12 @@ class DiscriminatorS(torch.nn.Module):
 
 
 class MultiPeriodDiscriminator(torch.nn.Module):
-    def __init__(self, use_spectral_norm=False):
+    def __init__(self):
         super(MultiPeriodDiscriminator, self).__init__()
         periods = [2, 3, 5, 7, 11]
 
-        discs = [DiscriminatorS(use_spectral_norm=use_spectral_norm)]
-        discs = discs + [DiscriminatorP(i, use_spectral_norm=use_spectral_norm) for i in periods]
+        discs = [DiscriminatorS()]
+        discs = discs + [DiscriminatorP(i) for i in periods]
         self.discriminators = nn.ModuleList(discs)
 
     def forward(self, y, y_hat):
