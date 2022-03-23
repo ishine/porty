40dd88198cac112dd8f8b2cb57a822f11ca1494b
pc3090 <ilikeniku@icloud.com>
2022-03-24 02:18:13 +0900

    initial

--- src/modules/models/transformer.py
+++ src/modules/models/transformer.py
@@ -65,6 +65,93 @@ class SelfAttention(nn.Module):
 
         if window_size is not None:
             rel_stddev = self.inter_channels ** -0.5
+            self.emb_rel_k = nn.Parameter(torch.randn(1, window_size * 2 + 1, self.inter_channels) * rel_stddev)
+            self.emb_rel_v = nn.Parameter(torch.randn(1, window_size * 2 + 1, self.inter_channels) * rel_stddev)
+
+        nn.init.xavier_uniform_(self.conv_q.weight)
+        nn.init.xavier_uniform_(self.conv_k.weight)
+        nn.init.xavier_uniform_(self.conv_v.weight)
+
+    def forward(self, x, c, attn_mask=None):
+        q = self.conv_q(x)
+        k = self.conv_k(c)
+        v = self.conv_v(c)
+
+        x = self.attention(q, k, v, mask=attn_mask)
+
+        x = self.conv_o(x)
+        return x
+
+    def attention(self, query, key, value, mask=None):
+        B, C, T = query.size()
+        query = query.view(B, self.n_heads, self.inter_channels, T).transpose(2, 3)
+        key = key.view(B, self.n_heads, self.inter_channels, T).transpose(2, 3)
+        value = value.view(B, self.n_heads, self.inter_channels, T).transpose(2, 3)
+
+        scores = torch.matmul(query / math.sqrt(self.k_channels), key.transpose(-2, -1))
+        if self.window_size is not None:
+            k_emb = self._get_relative_embeddings(self.emb_rel_k, T)
+            rel_logits = torch.matmul(query / math.sqrt(self.k_channels), k_emb.unsqueeze(0).transpose(-2, -1))
+            scores_local = self._relative_position_to_absolute_position(rel_logits)
+            scores = scores + scores_local
+        if mask is not None:
+            scores = scores.masked_fill(mask == 0, -1e8)
+        p_attn = F.softmax(scores, dim=-1)  # [b, n_h, t_t, t_s]
+        p_attn = self.drop(p_attn)
+        output = torch.matmul(p_attn, value)
+        if self.window_size is not None:
+            relative_weights = self._absolute_position_to_relative_position(p_attn)
+            v_emb = self._get_relative_embeddings(self.emb_rel_v, T)
+            output = output + torch.matmul(relative_weights, v_emb.unsqueeze(0))
+        output = output.transpose(2, 3).contiguous().view(B, C, T)
+        return output
+
+    def _get_relative_embeddings(self, relative_embeddings, length):
+        # Pad first before slice to avoid using cond ops.
+        pad_length = max(length - (self.window_size + 1), 0)
+        slice_start_position = max((self.window_size + 1) - length, 0)
+        slice_end_position = slice_start_position + 2 * length - 1
+        if pad_length > 0:
+            padded_relative_embeddings = F.pad(
+                relative_embeddings,
+                convert_pad_shape([[0, 0], [pad_length, pad_length], [0, 0]]))
+        else:
+            padded_relative_embeddings = relative_embeddings
+        used_relative_embeddings = padded_relative_embeddings[:, slice_start_position:slice_end_position]
+        return used_relative_embeddings
+
+    def _relative_position_to_absolute_position(self, x):
+        B, H, L, _ = x.size()
+        # Concat columns of pad to shift from relative to absolute indexing.
+        x = F.pad(x, convert_pad_shape([[0, 0], [0, 0], [0, 0], [0, 1]]))
+
+        # Concat extra elements so to add up to shape (len+1, 2*len-1).
+        x_flat = x.view([B, H, L * 2 * L])
+        x_flat = F.pad(x_flat, convert_pad_shape([[0, 0], [0, 0], [0, L - 1]]))
+
+        # Reshape and slice out the padded elements.
+        x_final = x_flat.view([B, H, L + 1, 2 * L - 1])[:, :, :L, L - 1:]
+        return x_final
+
+
+class SelfAttention(nn.Module):
+    def __init__(self, channels, n_heads, dropout=0., window_size=None):
+        super().__init__()
+        assert channels % n_heads == 0
+
+        self.inter_channels = channels // n_heads
+        self.n_heads = n_heads
+        self.window_size = window_size
+        self.scale = math.sqrt(self.inter_channels)
+
+        self.conv_q = nn.Conv1d(channels, channels, 1)
+        self.conv_k = nn.Conv1d(channels, channels, 1)
+        self.conv_v = nn.Conv1d(channels, channels, 1)
+        self.conv_o = nn.Conv1d(channels, channels, 1)
+        self.drop = nn.Dropout(dropout)
+
+        if window_size is not None:
+            rel_stddev = self.inter_channels ** -0.5
             self.emb_rel_k = nn.Parameter(torch.randn(1, self.inter_channels, window_size * 2 + 1) * rel_stddev)
             self.emb_rel_v = nn.Parameter(torch.randn(1, self.inter_channels, window_size * 2 + 1) * rel_stddev)
 
