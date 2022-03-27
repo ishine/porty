4146233884fd52ff84c1110d5ee4dbd4db0d8013
pc3090 <ilikeniku@icloud.com>
2022-03-28 00:00:38 +0900

    initial

--- src/preprocess.py
+++ src/preprocess.py
@@ -8,7 +8,7 @@ def main():
     parser.add_argument('-c', '--config', type=str, required=True)
     args = parser.parse_args()
     config = OmegaConf.load(args.config)
-    preprocessor = PreProcessor.from_config(config)
+    preprocessor = PreProcessor(config)
     preprocessor.run()
 
 
