b6c55c675f335c3e1918c34271d4029582fdadcc
pc3090 <ilikeniku@icloud.com>
2022-03-28 13:49:15 +0900

    initial

--- src/modules/trainer.py
+++ src/modules/trainer.py
@@ -31,7 +31,7 @@ class Trainer:
         self.generator = VITS(config.model).to(self.device)
         self.discriminator = MultiPeriodDiscriminator().to(self.device)
 
-        self.to_mel = MelSpectrogram(**config.mel).to(self.device)
+        self.to_mel = MelSpectrogram(config.mel).to(self.device)
 
     def run(self):
         seed_everything(self.config.seed)
